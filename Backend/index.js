861b123c1afe5331b538bc0f8b3e3f22d84f0e3c
Archita-Rai <raiarchita98@gmail.com>
2025-07-25 21:38:52 +0530

    fetching of data from the dashboard is completed

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-
 const mongoose = require("mongoose");
 const { HoldingModel } = require("./models/HoldingModel");
 const { PositionModel } = require("./models/PositionModel");
+const bodyParser = require('body-parser');
+const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3002;
 const URL = process.env.MONGO_URL;
 
 main()
@@ -146,46 +151,55 @@ async function main() {
 //   res.send("Done");
 // });
 
-app.get("/addPositions", (req, res) => {
-  let tempPositions = [
-    {
-      product: "CNC",
-      name: "EVEREADY",
-      qty: 2,
-      avg: 316.27,
-      price: 312.35,
-      net: "+0.58%",
-      day: "-1.24%",
-      isLoss: true,
-    },
-    {
-      product: "CNC",
-      name: "JUBLFOOD",
-      qty: 1,
-      avg: 3124.75,
-      price: 3082.65,
-      net: "+10.04%",
-      day: "-1.35%",
-      isLoss: true,
-    },
-  ];
+// app.get("/addPositions", (req, res) => {
+//   let tempPositions = [
+//     {
+//       product: "CNC",
+//       name: "EVEREADY",
+//       qty: 2,
+//       avg: 316.27,
+//       price: 312.35,
+//       net: "+0.58%",
+//       day: "-1.24%",
+//       isLoss: true,
+//     },
+//     {
+//       product: "CNC",
+//       name: "JUBLFOOD",
+//       qty: 1,
+//       avg: 3124.75,
+//       price: 3082.65,
+//       net: "+10.04%",
+//       day: "-1.35%",
+//       isLoss: true,
+//     },
+//   ];
 
-  tempPositions.forEach((item) => {
-    newPositions = new PositionModel({
-      product: item.product,
-      name: item.name,
-      qty: item.qty,
-      avg: item.avg,
-      price: item.price,
-      net: item.net,
-      day: item.day,
-      isLoss:item.isLoss,
-    });
-    newPositions.save()
-  });
-  res.send("done")
+//   tempPositions.forEach((item) => {
+//     newPositions = new PositionModel({
+//       product: item.product,
+//       name: item.name,
+//       qty: item.qty,
+//       avg: item.avg,
+//       price: item.price,
+//       net: item.net,
+//       day: item.day,
+//       isLoss:item.isLoss,
+//     });
+//     newPositions.save()
+//   });
+//   res.send("done")
+// });
+
+app.get("/allHoldings",async(req,res)=>{
+  let allHoldings = await HoldingModel.find({})
+  res.json(allHoldings)
 });
 
+app.get("/allPositions",async(req,res)=>{
+  let allPositions = await PositionModel.find({})
+  res.json(allPositions);
+})
 app.listen(PORT, (req, res) => {
   console.log("app is lestening on the port 8080");
 });
